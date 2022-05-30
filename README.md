788e2dae7a9dc229dfd805237703c15a51197d11
Elaina <userelaina@pm.me>
2022-05-30 12:05:07 +0000

    v3

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # collect
+
+```sh
+git submodule add git@liyu.utad.pt:happy-family/collect.git
+pip install -r collect/requirements.txt
+```
--- collect.py
+++ collect.py
@@ -1,12 +1,104 @@
-import os
-import time
-DIR=os.path.abspath(os.path.dirname(__file__))
-CSV=os.path.join(DIR,'1.csv')
+import sys
+import json
+import asyncio
+from bleak import BleakClient
 
-data=open(CSV,'r').read().split('\n')
-n=len(data)
-i=0
-while True:
-    print(data[i],flush=True)
-    i=(i+1)%n
-    time.sleep(1/20)
+DEBUG=False
+if len(sys.argv)>1 and sys.argv[1]=='--debug':
+    DEBUG=True
+
+cfg=json.load(open('collect.json'))
+alpha=cfg['alpha']
+devices=[cfg[i] for i in cfg['collect']]
+
+cache={}
+cache_flag={i:False for i in devices}
+
+def f(addr:str,data:bytes)->None:
+    assert len(data)==20
+    (ox55,ox61,axL,axH,ayL,ayH,azL,azH,wxL,wxH,wyL,wyH,wzL,wzH,RollL,RollH,PitchL,PitchH,YawL,YawH)=data
+    assert ox55==0x55
+    assert ox61==0x61
+
+    def int2(h:int,l:int)->int:
+        ans=(h<<8)|l
+        if ans>32767:
+            ans-=65536
+        return ans
+
+    ax=int2(axH,axL)
+    ay=int2(ayH,ayL)
+    az=int2(azH,azL)
+    wx=int2(wxH,wxL)
+    wy=int2(wyH,wyL)
+    wz=int2(wzH,wzL)
+    Roll=int2(RollH,RollL)
+    Pitch=int2(PitchH,PitchL)
+    Yaw=int2(YawH,YawL)
+
+    def linear(x:int,k:int)->float:
+        return k*x/32768
+
+    ax=linear(ax,16)
+    ay=linear(ay,16)
+    az=linear(az,16)
+    wx=linear(wx,2000)
+    wy=linear(wy,2000)
+    wz=linear(wz,2000)
+    Roll=linear(Roll,180)
+    Pitch=linear(Pitch,180)
+    Yaw=linear(Yaw,180)
+
+    agx=ax
+    agy=ay
+    agz=az
+    alx=0
+    aly=0
+    alz=0
+
+    if addr in cache:
+        (lax,lay,laz,lwx,lwy,lwz,lRoll,lPitch,lYaw,lalx,laly,lalz,lagx,lagy,lagz)=cache[addr]
+        agx=alpha*lagx+(1-alpha)*ax
+        agy=alpha*lagy+(1-alpha)*ay
+        agz=alpha*lagz+(1-alpha)*az
+        alx=ax-agx
+        aly=ay-agy
+        alz=az-agz
+
+    cache[addr]=(ax,ay,az,wx,wy,wz,Roll,Pitch,Yaw,alx,aly,alz,agx,agy,agz)
+    cache_flag[addr]=True
+
+    if not all(cache_flag.values()):
+        return
+    for i in devices:
+        print(','.join([str(j) for j in cache[i]]),end='\n' if i is devices[-1] else ',')
+        cache_flag[i]=False
+    sys.stdout.flush()
+
+
+def notification_handler(id):
+    def handler(sender,data):
+        f(id,data)
+    return handler
+
+async def run(id):
+    _count=0
+    while True:
+        try:
+            if DEBUG:
+                print("Connecting to: {}".format(id))
+            client = BleakClient(id)
+            await client.connect()
+            await client.start_notify(cfg['imuReadUUID'], notification_handler(id))
+            _count=0
+        except:
+            _count+=1
+            if _count>=10:
+                print("Failed to connect to {}".format(id))
+                sys.exit(0)
+
+async def main():
+    tasks=[run(i) for i in devices]
+    await asyncio.gather(*tasks)
+
+asyncio.run(main())
